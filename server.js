7f3708cea6c8dc398a0f5aaf248835645bc9d067
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 20:48:21 -0700

    Import data file and server on json route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dataObject = require('./data');
 
 const urlLogger = (request, response, next) => {
   console.log('Request URL:', request.url);
@@ -16,7 +17,7 @@ app
   .use(timeLogger, urlLogger)
   .use(express.static(path.join(__dirname, 'public')))
   .get('/json', (request, response) => {
-    response.status(200).json({ name: 'Hugh' });
+    response.status(200).json(data);
   })
   .use((request, response, next) => {
     response.status(404).send('There ain\'t no body home!')
