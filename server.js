ac2d14aea0a015eb894a95d231ae636e95537a85
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 20:03:18 -0700

    Build /json get request in server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 app
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (request, response) => {})
+  .get('/json', (request, response) => {
+    response.status(200).json({ name: 'Hugh' });
+  })
   .listen(3001, () => {
     console.log('Express intro running on localhost:3000');
   });
