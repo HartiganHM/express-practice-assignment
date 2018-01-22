0fc56ba622edb6f58fb614ee27933401dc665ea1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 19:59:25 -0700

    Serve static assets with server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (request, response) => {
-  response.send('hello world');
-}).listen(3001, () => {
-  console.log('Express intro running on localhost:3000');
-});
+app
+  .get('/', (request, response) => {
+    
+  })
+  .use(express.static(path.join(_dirname, 'public')))
+  .listen(3001, () => {
+    console.log('Express intro running on localhost:3000');
+  });
