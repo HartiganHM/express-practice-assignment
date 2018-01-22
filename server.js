e2d0c05f57bfb6f81586a912092f83b3c67b461c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 20:00:48 -0700

    Fix argument name

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = express();
 const path = require('path');
 
 app
-  .get('/', (request, response) => {
-    
-  })
-  .use(express.static(path.join(_dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (request, response) => {})
   .listen(3001, () => {
     console.log('Express intro running on localhost:3000');
   });
