f2c27a5ba12d2dd4194ceea947d1b4548da567b4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 20:22:24 -0700

    Build urlLogger middleware

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const urlLogger (request, response, next) => {
+  console.log('Request URL:', request.url);
+  next();
+};
+
 app
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/json', (request, response) => {
+  .get('/json', urlLogger, (request, response) => {
     response.status(200).json({ name: 'Hugh' });
   })
   .listen(3001, () => {
